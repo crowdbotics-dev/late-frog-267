a6ecf517d1dc5c625eda75c2b1ea127a221244ad
Crowdbotics <engineering@crowdbotics.com>
2024-04-02 07:11:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-frog-267",
+  url: "https://late-frog-267.botics.co",
+  api: "https://late-frog-267.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
